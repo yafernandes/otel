4646895baac711bf3afdcccc82156b021ce7a32d
Alex Fernandes <alex.fernandes@datadoghq.com>
2024-03-08 08:46:05 -0500

    Updated cert-manager version

--- init-cluster.sh
+++ init-cluster.sh
@@ -6,21 +6,19 @@ kubectl create ns otel
 
 kubectl apply -n otel -f datadog-secrets.yaml
 
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.2/cert-manager.yaml
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.4/cert-manager.yaml
 
 helm install opentelemetry-operator open-telemetry/opentelemetry-operator \
   -n otel \
   --set admissionWebhooks.certManager.enabled=false \
   --set admissionWebhooks.certManager.autoGenerateCert=true
 
-kubectl wait --namespace otel --for=condition=ready pod --selector=app.kubernetes.io/name=opentelemetry-operator
+kubectl wait -n otel --for=condition=ready pod --selector=app.kubernetes.io/name=opentelemetry-operator --timeout=5m
 
-kubectl apply -f auto-intrumentation.yaml
+kubectl apply -n otel -f auto-intrumentation.yaml
 
 helm install opentelemetry-collector open-telemetry/opentelemetry-collector -n otel -f otel-daemonset-values.yaml
 helm install opentelemetry-cluster-collector open-telemetry/opentelemetry-collector -n otel -f otel-deployment-values.yaml
 
 # No arm64 support - https://github.com/open-telemetry/opentelemetry-network/blob/main/docs/roadmap.md#arm-support
 # helm install opentelemetry-ebpf open-telemetry/opentelemetry-ebpf -n otel -f otel-ebpf-values.yaml
-
-kubectl apply -f workload/toolbox.yaml
